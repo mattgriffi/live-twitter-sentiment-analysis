2591a4b3c98845e9645f0e1bec3af9866818dece
mseffner <mseffner@gmail.com>
2017-06-16 13:25:04 -0700

    started major refactor

--- classifier_trainer.py
+++ classifier_trainer.py
@@ -1,103 +1,60 @@
 """This class will train the machine learning classifiers with a data set."""
 
-import csv
-import itertools
+import logging
 import nltk
-import os
-import os.path
-import pickle
-import random
 import time
-import unidecode
 
-from nltk.tokenize import word_tokenize
 from nltk.classify.scikitlearn import SklearnClassifier
 from sklearn.naive_bayes import MultinomialNB, BernoulliNB
 from sklearn.linear_model import LogisticRegression, SGDClassifier
 from sklearn.svm import LinearSVC, NuSVC
 
+from data import DataSet
 
-class DataSet:
+logging.basicConfig(level=logging.DEBUG,
+                    format=' %(asctime)s - %(levelname)s - %(funcName)-30s - %(message)s')
 
-    data_set = None
 
-    def __init__(self):
-        self.training_set = None
-        self.all_features = None
-        self.test_set = None
+class ClassifierTrainer:
 
-    @staticmethod
-    def get_data():
-        if DataSet.data_set is None:
-            print("Loading data set from scratch...")
-            DataSet.data_set = DataSet._load_data()
-        print("Returning cached data set.")
-        return DataSet.data_set
+    trained_classifiers = []
 
     @staticmethod
-    def _load_data():
-        """Loads the data from all corpora. Returns a DataSet object, which contains the training
-        set and the list of all words that appear in the corpora."""
-
-        # Load the data set from pickle if it exists
-        pickle_filepath = os.path.join('pickles', 'data.pickle')
-        if os.path.isfile(pickle_filepath):
-            print("Loading data from pickle...")
-            with open(pickle_filepath, 'rb') as pickle_file:
-                data = pickle.load(pickle_file)
-            return data
-
-        start_time = time.time()
-
-        documents = []
-
-        # Load the short pos/neg movie reviews (approx 10,000)
-        print("Loading movie reviews...")
-        short_pos = unidecode.unidecode(open('positive.txt').read())
-        short_neg = unidecode.unidecode(open('negative.txt').read())
-        for review in short_pos.split('\n'):
-            documents.append((review, 'pos'))
-        for review in short_neg.split('\n'):
-            documents.append((review, 'neg'))
-
-        # Build list of all words that appear in data set
-        print("Building word list...")
-        all_words = [word_tokenize(feature[0]) for feature in documents]
-        all_words = list({word.lower() for word in itertools.chain.from_iterable(all_words)
-                         if len(word) > 2})
-
-        # Build list of labeled features
-        print("Building featureset...")
-        labeled_features = [(DataSet.find_features(feature, all_words), sentiment)
-                            for feature, sentiment in documents]
-
-        # Shuffle the featuresets
-        random.shuffle(labeled_features)
-
-        # Create the DataSet object and store the data in it
-        data = DataSet()
-        data.training_set = labeled_features[:10000]
-        data.all_features = all_words
-        data.test_set = labeled_features[10000:]
-
-        # Pickle the data set to reduce future loading times
-        if not os.path.isdir('pickles'):
-            os.mkdir('pickles')
-        with open(pickle_filepath, 'wb') as pickle_file:
-            print("Writing pickle file...")
-            pickle.dump(data, pickle_file, protocol=pickle.HIGHEST_PROTOCOL)
-
-        print(f"Data loading complete. Time taken: {time.time()-start_time}\n")
-        return data
+    def get_trained_classifiers(classifier_list):
+        """Returns a list of trained classifiers. classifier_list is a list of machine learning
+        classifier constructor functions."""
 
     @staticmethod
-    def find_features(document, word_features):
-        doc_words = set(word.lower() for word in word_tokenize(document))
-        return {word: (word in doc_words) for word in word_features}
+    def _get_named_classifiers(classifier_list):
+        """Returns a list of NamedClassifiers."""
+
+        return [NamedClassifier(classifier) for classifier in classifier_list]
+
+    @staticmethod
+    def _wrap_named_classifiers(named_classifier_list):
+        """Takes a list of NamedClassifiers and wraps each of them with SklearnClassifier."""
+
+        for named_classifier in named_classifier_list:
+            named_classifier.classifier = SklearnClassifier(named_classifier.classifier)
+
+    @staticmethod
+    def _create_and_train_algorithms(wrapped_named_classifier_list, training_set):
+        """Takes a list of SklearnClassifier-wrapped NamedClassifiers and trains each of
+        them."""
+
+        for named_classifier in wrapped_named_classifier_list:
+            logging.info(f"Training {named_classifier.name}...")
+            start = time.time()
+            named_classifier.classifier.train(training_set)
+            logging.info(f"Training complete. Time taken: {time.time()-start}\n")
 
 
-def train_classifier(classifier, labeled_features):
-    classifier.train(labeled_features)
+class NamedClassifier:
+    """This class stores a classifier and its name."""
+
+    def __init__(self, classifier):
+        self.classifier = classifier
+        self.name = classifier.__class__.__name__
 
 
 def test_algorithm_accuracy(algorithm_list, testing_set):
@@ -109,19 +66,6 @@ def test_algorithm_accuracy(algorithm_list, testing_set):
         for i in range(10):
             print(f"Test {i}: {algorithm._clf.__class__.__name__} -> "
                   f"{algorithm.classify(testing_set[i][0])} : {testing_set[i][1]}")
-
-
-def create_and_train_algorithms(algorithm_list, training_set):
-    """Takes a list of machine learning algorithm constructor functions. Instantiates each of
-    the algorithms, trains them with the given training set, and returns the list of trained
-    algorithms."""
-    training_list = [SklearnClassifier(algorithm()) for algorithm in algorithm_list]
-    for algorithm in training_list:
-        print(f"Training {algorithm._clf.__class__.__name__}...")
-        start = time.time()
-        algorithm.train(training_set)
-        print(f"Training complete. Time taken: {time.time()-start}\n")
-    return training_list
 
 
 data = DataSet.get_data()
