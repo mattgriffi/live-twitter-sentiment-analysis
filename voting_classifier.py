9bbba4866e493e7e6c4a039eacbce3381b2d8b68
mseffner <mseffner@gmail.com>
2017-06-17 08:43:23 -0700

    basic VotingClassifier implementation

--- voting_classifier.py
+++ voting_classifier.py
@@ -2,9 +2,36 @@
 classify a text, then return a majority vote of the classifications determined by the multiple
 classifiers."""
 
+import statistics
 
 from nltk import ClassifierI
+from sklearn.naive_bayes import MultinomialNB, BernoulliNB
+from sklearn.linear_model import LogisticRegression, SGDClassifier
+from sklearn.svm import LinearSVC
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neural_network import MLPClassifier
+
+from classifier_trainer import ClassifierTrainer
 
 
 class VotingClassifier(ClassifierI):
-    pass
+
+    def __init__(self):
+        self.classifiers = self._get_classifiers()
+        self.confidence = None
+
+    def _get_classifiers(self):
+        classifier_list = [MultinomialNB, BernoulliNB, LogisticRegression, SGDClassifier,
+                           LinearSVC, DecisionTreeClassifier, MLPClassifier]
+        return ClassifierTrainer.get_trained_classifiers(classifier_list)
+
+    def classify(self, featureset):
+        results = []
+        for classifier in self.classifiers:
+            results.append(classifier.classify(featureset))
+        mode = statistics.mode(results)
+        self.confidence = mode / len(results)
+        return mode
+
+    def get_most_recent_confidence(self):
+        return self.confidence
